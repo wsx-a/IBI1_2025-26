3b8b86d2fc6849e32ff91fadf28d2d5e3c1fecf7
wsx-a <2839611988@qq.com>
2026-03-18 14:46:22 +0800

    modified:   practical5/gene.py

--- practical5/gene.py
+++ practical5/gene.py
@@ -1,3 +1,15 @@
 genes = {'TP53':12.4, 'EGFR':15.1, 'BRCA1':8.2, 'PTEN':5.3, 'ESR1':10.7}
 genes['MYC'] = 11.6
 import matplotlib.pyplot as plt
+plt.bar(genes.keys(), genes.values())
+plt.xlabel("Gene")
+plt.ylabel("Expression")
+plt.title("Gene Expression Levels")
+plt.show()
+target_gene in genes = 'TP53'
+if target_gene in genes:
+    print(target_gene, 'exprssion value is', genes[target_gene])
+else:
+    print('Error: gene not fount')
+average = sum(genes.values()) / len(genes)
+print('Average gene expression is', average)
