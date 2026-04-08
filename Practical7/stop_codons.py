4c545d083ffdd0a0d79f56ea73ba3d82fa3dff53
wsx-a <2839611988@qq.com>
2026-04-08 12:33:00 +0800

    modified:   Practical7/stop_codons.py 	new file:   stop_genes.fa

--- Practical7/stop_codons.py
+++ Practical7/stop_codons.py
@@ -1,9 +1,11 @@
-input_file = "Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa"
+input_file = "E:/IBI1/IBI1_2025-26/Practical7/Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa"
 output_file = "stop_genes.fa"
 stop_codons = ["TAA", "TAG", "TGA"]
+
 genes = []
 header = ""
 sequence = ""
+
 with open(input_file, "r") as f:
     for line in f:
         line = line.strip()
@@ -13,39 +15,34 @@ with open(input_file, "r") as f:
             header = line
             sequence = ""
         else:
-            squence = sequence + line
+            sequence = sequence + line
     if header != "":
         genes.append((header, sequence))
+
 def get_gene_name(header_line):
     parts = header_line.split()
     for part in parts:
         if part.startswith("gene:"):
             return part.replace("gene:", "")
     return parts[0].replace(">", "")
+
 def find_in_frame_stops(seq):
     found_stops = set()
-    for i in range(len(seq) - 2):
-        if seq[i:i+3] == "ATG":
-            # read codons from this ATG
-            for j in range(i, len(seq) - 2, 3):
-                codon = seq[j:j+3]
-                if codon in stop_codons:
-                    found_stops.add(codon)
-                    break
+    for frame in range(3):
+        for i in range(frame, len(seq) - 2, 3):
+            codon = seq[i:i+3]
+            if codon in stop_codons:
+                found_stops.add(codon)
     return sorted(found_stops)
+
 with open(output_file, "w") as out:
     for header, seq in genes:
         gene_name = get_gene_name(header)
         found = find_in_frame_stops(seq)
         if len(found) > 0:
             stop_text = ",".join(found)
-            out.write(">" + gene_name + " " + stop_text + "\n")
-            out.write(seq + "\n")
+            out.write(f">{gene_name} {stop_text}\n")
+            out.write(f"{seq}\n")
+
 print("Finished.")
 print("Output written to:", output_file)
-
-
-
-
-
-
